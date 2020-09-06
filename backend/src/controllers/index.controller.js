b4967204f486355a6f7641730c33456d1116d423
Diego Betancourt <diego.betancourt@jikkosoft.com>
2020-09-05 19:12:33 -0500

    structure of database and files

--- backend/src/controllers/index.controller.js
+++ backend/src/controllers/index.controller.js
@@ -16,12 +16,6 @@ const getUsers = async (req, res) => {
     res.status(200).json(response.rows);
 };
 
-const getUserById = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const response = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-    res.json(response.rows);
-};
-
 const createUser = async (req, res) => {
     const { name, dad, mom } = req.body;
     const response = await pool.query('INSERT INTO users (name, dad, mom) VALUES ($1, $2, $3)', [name, dad, mom]);
@@ -33,30 +27,7 @@ const createUser = async (req, res) => {
     })
 };
 
-const updateUser = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const { name, email } = req.body;
-
-    const response =await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [
-        name,
-        email,
-        id
-    ]);
-    res.json('User Updated Successfully');
-};
-
-const deleteUser = async (req, res) => {
-    const id = parseInt(req.params.id);
-    await pool.query('DELETE FROM users where id = $1', [
-        id
-    ]);
-    res.json(`User ${id} deleted Successfully`);
-};
-
 module.exports = {
     getUsers,
-    getUserById,
     createUser,
-    updateUser,
-    deleteUser
 };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,12 +3,9 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/index.controller');
+const { getUsers, createUser} = require('../controllers/index.controller');
 
 router.get('/users', getUsers);
-router.get('/users/:id', getUserById);
 router.post('/users', createUser);
-router.put('/users/:id', updateUser)
-router.delete('/users/:id', deleteUser);
 
 module.exports = router;    
